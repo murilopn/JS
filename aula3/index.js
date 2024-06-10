621145d16f0d171cb310db3817c342a8a957b3c6
murilopn <murilopatricionascimento@gmail.com>
2024-06-10 12:18:29 +0000

    first commit

---
+++ aula3/index.js
@@ -0,0 +1,7 @@
+console.log('Murilo');
+console.log("Murilo");
+console.log(`Murilo`);
+console.log(35, 1312.343, 'Murilo')
+//Em java posso colocar ou não ponto e vírgula ;
+//Não existe e float em Java, tratamos como tipo Number
+//log é o método e console a classe
